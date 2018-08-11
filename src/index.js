6ac912e832cb1800ffb5ed9e60f8c18cdba8ebe1
wengjq <2410808055@qq.com>
2018-08-12 00:02:44 +0800

    update eslint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
+/* global document:true */
+/* eslint no-undef: "error" */
 import './styles.css';
 import './animation.css';
-import Carousel from './carousel.js';
+import Carousel from './carousel';
 
-let carouselContainer = document.getElementById('carousel-container');
+const carouselContainer = document.getElementById('carousel-container');
 
-new Carousel(carouselContainer, {
-    'autoPlay': true, // 自动播放
-    'currentIndex': 0, // 开始的帧数
-    'speed': 1500, // 切换的速度
-    'duration': 4000, // 停留的时间
-    'dots': true,
-    'arrows': true,
-    'effectIndex': 13 // 切换的效果(1：淡入淡出,2：水平滑动,3：垂直滑动,4：水平翻转,5：垂直翻转,6：水平 3D 翻转,7：垂直 3D 翻转, 8: 淡入放大,9：水平魔方旋转,10：垂直魔方旋转,11： 聚焦,12：分区聚焦,13： 顺时针风车,14：逆时针风车)
+const carouselInstance = new Carousel(carouselContainer, {
+  autoPlay: true, // 自动播放
+  currentIndex: 0, // 开始的帧数
+  speed: 1500, // 切换的速度
+  duration: 4000, // 停留的时间
+  dots: true,
+  arrows: true,
+  effectIndex: 13,
+  // 切换的效果
+  // 1：淡入淡出, 2：水平滑动, 3：垂直滑动, 4：水平翻转,
+  // 5：垂直翻转, 6：水平 3D 翻转, 7：垂直 3D 翻转, 8: 淡入放大,
+  // 9：水平魔方旋转, 10：垂直魔方旋转, 11： 聚焦, 12：分区聚焦,
+  // 13： 顺时针风车, 14：逆时针风车
 });
+
+carouselInstance();
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,152 +1,139 @@
-export function _extend(dst, src) {
-    if (dst && src) {
-        for (let key in src) {
-            if (src.hasOwnProperty(key)) {
-                dst[key] = src[key];
-            }
-        }
+/* global document:true */
+/* eslint no-undef: "error" */
+export function extend(dst, src) {
+  if (dst && src) {
+    for (const key in src) {
+      if (Object.prototype.hasOwnProperty.call(src, key)) {
+        dst[key] = src[key];
+      }
+    }
+  }
+  return dst;
+}
+
+export function getElementWidth(el) {
+  return el.getBoundingClientRect().width || el.offsetWidth;
+}
+
+export function getElementHeight(el) {
+  return el.getBoundingClientRect().height || el.offsetHeight;
+}
+
+export function css(el, prop, val) {
+  const style = el && el.style;
+
+  if (style) {
+    if (typeof val === 'undefined') {
+      if (document.defaultView && document.defaultView.getComputedStyle) {
+        val = document.defaultView.getComputedStyle(el, '');
+      } else if (el.currentStyle) {
+        val = el.currentStyle;
+      }
+
+      return (typeof prop === 'undefined') ? val : val[prop];
     }
 
-    return dst;
-}
-
-function hasClass(element, cName) {
-    return !!element.className.match(new RegExp("(\\s|^)" + cName + "(\\s|$)"));
-};
-
-export function addClass(element, cName) {
-
-    if (!hasClass(element, cName)) {
-        element.className += " " + cName;
+    if (!(prop in style)) {
+      prop = `-webkit-${prop}`;
     }
+
+    style[prop] = val + (typeof val === 'string' ? '' : 'px');
+  }
+
+  return undefined;
 }
 
-export function removeClass(element, cName) {
-    if (hasClass(element, cName)) {
-        element.className = element.className.replace(new RegExp("(\\s|^)" + cName + "(\\s|$)"), " ");
-    }
+export function vendorCss(el, prop, val) {
+  css(el, `Webkit${prop}`, val);
+  css(el, `Moz${prop}`, val);
+  css(el, `ms${prop}`, val);
+  css(el, `O${prop}`, val);
+
+  css(el, prop, val);
 }
 
-export function _getElementWidth(el) {
-    return el.getBoundingClientRect().width || el.offsetWidth;
+export function cssList(elements, prop, val) {
+  [...elements].forEach((item) => {
+    css(item, prop, val);
+  });
 }
 
-export function _getElementHeight(el) {
-    return el.getBoundingClientRect().height || el.offsetHeight;
-}
-
-export function _vendorCss(el, prop, val) {
-    _css(el, `Webkit${prop}`, val);
-    _css(el, `Moz${prop}`, val);
-    _css(el, `ms${prop}`, val);
-    _css(el, `O${prop}`, val);
-
-    _css(el, prop, val);
-}
-
-export function _cssList(elements, prop, val) {
-    [...elements].forEach((item) => {
-        _css(item, prop, val);
-    });
-}
-
-export function _css(el, prop, val) {
-    let style = el && el.style;
-
-    if (style) {
-        if (val === void 0) {
-            if (document.defaultView && document.defaultView.getComputedStyle) {
-                val = document.defaultView.getComputedStyle(el, '');
-            } else if (el.currentStyle) {
-                val = el.currentStyle;
-            }
-
-            return prop === void 0 ? val : val[prop];
-        } else {
-            if (!(prop in style)) {
-                prop = '-webkit-' + prop;
-            }
-
-            style[prop] = val + (typeof val === 'string' ? '' : 'px');
-        }
-    }
-}
 
 export function addAnimationEndEvent(elem, endEvent) {
-    elem.addEventListener('animationend', endEvent, false);
-    elem.addEventListener('webkitAnimationEnd', endEvent, false);
-    elem.addEventListener('oAnimationEnd', endEvent, false);
-    elem.addEventListener('oanimationend', endEvent, false);
-    elem.addEventListener('msAnimationEnd', endEvent, false);
+  elem.addEventListener('animationend', endEvent, false);
+  elem.addEventListener('webkitAnimationEnd', endEvent, false);
+  elem.addEventListener('oAnimationEnd', endEvent, false);
+  elem.addEventListener('oanimationend', endEvent, false);
+  elem.addEventListener('msAnimationEnd', endEvent, false);
 
-    return true;
+  return true;
 }
 
 export function removeAnimationEndEvent(elem, endEvent) {
-    elem.removeEventListener('animationend', endEvent, false);
-    elem.removeEventListener('webkitAnimationEnd', endEvent, false);
-    elem.removeEventListener('oAnimationEnd', endEvent, false);
-    elem.removeEventListener('oanimationend', endEvent, false);
-    elem.removeEventListener('msAnimationEnd', endEvent, false);
+  elem.removeEventListener('animationend', endEvent, false);
+  elem.removeEventListener('webkitAnimationEnd', endEvent, false);
+  elem.removeEventListener('oAnimationEnd', endEvent, false);
+  elem.removeEventListener('oanimationend', endEvent, false);
+  elem.removeEventListener('msAnimationEnd', endEvent, false);
 
-    return true;
+  return true;
 }
 
 export const checkBrowser = (function() {
-    let animationProperties = ['animation', 'MozAnimation', 'webkitAnimation', 'msAnimation', 'OAnimation'];
-    let transformProperties = ['transform', 'MozTransform', 'webkitTransform', 'msTransform', 'OTransform'];
-    let opacityProperties = ['opacity', 'MozOpacity', 'webkitOpacity', 'msOpacity', 'OOpacity'];
-    let div = document.createElement('div');
-    let props;
-    let result = {};
+  const animationProperties = ['animation', 'MozAnimation', 'webkitAnimation', 'msAnimation', 'OAnimation'];
+  const transformProperties = ['transform', 'MozTransform', 'webkitTransform', 'msTransform', 'OTransform'];
+  const opacityProperties = ['opacity', 'MozOpacity', 'webkitOpacity', 'msOpacity', 'OOpacity'];
+  const div = document.createElement('div');
+  let props;
+  const result = {};
 
-    return function(flag) {
-        if (typeof result[flag] == "boolean") {
-            return result[flag];
-        }
+  return function(flag) {
+    if (typeof result[flag] === 'boolean') {
+      return result[flag];
+    }
 
-        switch (flag) {
-            case "animation":
-                props = animationProperties;
-                break;
-            case "transform":
-                props = transformProperties;
-                break;
-            case "opacity":
-                props = opacityProperties;
-                break;
-            default:
-                props = animationProperties;
-        }
+    switch (flag) {
+      case 'animation':
+        props = animationProperties;
+        break;
+      case 'transform':
+        props = transformProperties;
+        break;
+      case 'opacity':
+        props = opacityProperties;
+        break;
+      default:
+        props = animationProperties;
+    }
 
-        for (let i = 0, len = props.length; i < len; i++) {
-            if (div.style[props[i]] !== undefined) {
-                result[flag] = true;
-
-                return result[flag];
-            }
-        }
-
-        result[flag] = false;
+    for (let i = 0, len = props.length; i < len; i += 1) {
+      if (div.style[props[i]] !== undefined) {
+        result[flag] = true;
 
         return result[flag];
+      }
     }
-})();
+
+    result[flag] = false;
+
+    return result[flag];
+  };
+}());
 
 export function parseDom(nodelist) {
-    let $div = document.createElement("div");
+  const $div = document.createElement('div');
 
-    $div.innerHTML = nodelist;
+  $div.innerHTML = nodelist;
 
-    return $div.childNodes[0];
+  return $div.childNodes[0];
 }
 
 export function insertAfter(newElement, targetElement) {
-    var parent = targetElement.parentNode;
+  var parent = targetElement.parentNode;
 
-    if (parent.lastChild == targetElement) {
-        parent.appendChild(newElement);
-    } else {
-        parent.insertBefore(newElement, targetElement.nextSibling);
-    }
+  if (parent.lastChild === targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement, targetElement.nextSibling);
+  }
 }
